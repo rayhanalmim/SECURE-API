8e7f4db662734d5d5822c3756537513fd3baea99
rayhanalmim <rayhanalmim1@gmail.com>
2023-10-28 21:00:30 +0600

    first commit

---
+++ secure.js
@@ -0,0 +1,14 @@
+/**
+ * To get started, first install the JSON Web Token (JWT) library.
+ * Create an API endpoint and make sure to access it after a user signs in or signs up.
+ * On the client-side, install Axios and use it to make a POST request to send user information to the API.
+ * Generate a JWT token with jwt.sign, specifying the user, a secret, and an expiration time (e.g., { expiresIn: '4h' }).
+ * Send this token to the client and store it in cookies (recommended), or you can also store it on the local server or in memory/database.
+ * To store the token in cookies, you need to consider some conditions. If the server and client are running on different ports, you should configure CORS on the server (e.g., origin: ['serverURL'], credentials: true), and on the client-side, use 'withCredentials' in Axios.
+ * Send the cookies to the server when a user try to access a route which is you want to secure.
+ * To receive cookies in an API, you must use the 'cookieParser' middleware and call it inside 'app.use()'.
+ * To secure specific APIs, you should use 'withCredentials' with those Axios requests.
+ * Now, create a middleware to check if the token exists or not.
+ * If it exists, verify it using jwt.verify(token, secret, (err, decoded)). If there's an error, handle it; otherwise, assign the decoded token data to 'req.user'.
+ * Inside the API, check if the requested user matches the token user. If not, return a 403 (forbidden) status.
+ */
